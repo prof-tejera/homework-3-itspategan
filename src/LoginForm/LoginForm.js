2860f5fdad68f6ee89af012c43f10d86635cc631
itspategan <86270686+itspategan@users.noreply.github.com>
2021-09-30 21:49:01 -0400

    Login Form

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -1,9 +1,20 @@
-import { Component } from 'react';
-import './LoginForm.css';
+import { Component } from "react";
+import "./LoginForm.css";
+import Button from "components/Button/Button";
+import Input from "components/Input/Input";
 
 class LoginForm extends Component {
   render() {
-    return null;
+    return (
+      <div id="form-container">
+        <h1 id="login-header">Login</h1>
+        <form id="login-form">
+          <Input className="form-field" />
+          <Input className="form-field" />
+          <Button text="Login" />
+        </form>
+      </div>
+    );
   }
 }
 
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,13 @@
-import { Component } from 'react';
-import './Button.css';
+import { Component } from "react";
+import "./Button.css";
 
 class Button extends Component {
   render() {
-    return <button className="Default-button">{this.props.text}</button>;
+    return (
+      <button className="Default-button" type="submit">
+        {this.props.text}
+      </button>
+    );
   }
 }
 
